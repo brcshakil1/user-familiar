f8ee5cd7fc66ab27a5174d8a84ab70a48f3451b7
brcshakil1 <brcshakil72@gmail.com>
2022-08-01 11:36:01 +0600

    user familiar

---
+++ src/component/User/User.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const User = (props) => {
+    const familiar = props.familiar;
+    let greeting ;
+    if(familiar){
+        greeting = <p>Welcome, my friend.</p>;
+    }
+    else{
+        greeting = <p>Who are you?</p>;
+    }
+
+    let food = (familiar ? <p>I'll buy food</p> : <p>Let's eat his his whose whose</p>);
+    return (
+        <div>
+            <div>
+                <h2>Greetings</h2>
+                {greeting}
+            </div>
+            <div>
+                <h3>Food</h3>
+                {
+                    food
+                }
+                
+                
+            </div>
+            <div>
+                <h3>Connection</h3>
+                {
+                    familiar ? 
+                    <p>Let's join my facebook</p> 
+                    : <p>I don't need stranger</p>
+                }
+                
+                
+            </div>
+        </div>
+    );
+};
+
+export default User;
